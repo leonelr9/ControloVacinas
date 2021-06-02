57004b6c34d1449fce44f0107ec8c0a24afdf922
Leonel Rita <leonelrita@hotmail.com>
2021-06-02 16:21:56 +0100

    Classe para representar o paciente (Dados todos incluidos)

--- app/src/main/java/com/example/controlovacinas/Paciente.kt
+++ app/src/main/java/com/example/controlovacinas/Paciente.kt
@@ -1,6 +1,38 @@
 package com.example.controlovacinas
 
-import java.util.*
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-data class Paciente(var id:Long, var nome: String, var data_nascimento: Date, var sexo: String, var contacto: String){
+
+data class Paciente(var id:Long = -1, var nome: String, var data_nascimento: Long, var sexo: String, var contacto: String){
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply {
+            put(TabelaPaciente.NOME, nome)
+            put(TabelaPaciente.DATA_NASCIMENTO, data_nascimento)
+            put(TabelaPaciente.SEXO, sexo)
+            put(TabelaPaciente.CONTACTO, contacto)
+        }
+
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Paciente {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colNome = cursor.getColumnIndex(TabelaPaciente.NOME)
+            val colData = cursor.getColumnIndex(TabelaPaciente.DATA_NASCIMENTO)
+            val colSexo = cursor.getColumnIndex(TabelaPaciente.SEXO)
+            val colContacto = cursor.getColumnIndex(TabelaPaciente.CONTACTO)
+
+            val id = cursor.getLong(colId)
+            val nome = cursor.getString(colNome)
+            val data = cursor.getLong(colData)
+            val sexo = cursor.getString(colSexo)
+            val contacto = cursor.getString(colContacto)
+
+
+            return Paciente(id, nome, data, sexo, contacto)
+        }
+    }
 }

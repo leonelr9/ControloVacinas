edaafbd53d55fd8980a11c203ab8f05359ed3cbc
Leonel Rita <leonelrita@hotmail.com>
2021-06-02 14:53:13 +0100

    Criar tipo de dados para representar um paciente

---
+++ app/src/main/java/com/example/controlovacinas/Paciente.kt
@@ -0,0 +1,6 @@
+package com.example.controlovacinas
+
+import java.util.*
+
+data class Paciente(var id:Long, var nome: String, var data_nascimento: Date, var sexo: String, var contacto: String){
+}

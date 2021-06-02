09748abeb11d407b9ed19b16d135711bd3cfeb20
Leonel Rita <leonelrita@hotmail.com>
2021-06-02 17:04:04 +0100

    Conclusão da classe Vacina (ContentValues)

--- app/src/main/java/com/example/controlovacinas/Vacina.kt
+++ app/src/main/java/com/example/controlovacinas/Vacina.kt
@@ -1,6 +1,37 @@
 package com.example.controlovacinas
 
-import java.util.*
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-data class Vacina (var id: Long, var num_lote: String, var data_vacinacao: Date, var idPaciente: Long, var idFabricante: Long) {
+data class Vacina (var id: Long = -1, var num_lote: String, var data_vacinacao: Long, var idPaciente: Long, var idFabricante: Long) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply {
+            put(TabelaVacina.NUM_LOTE, num_lote)
+            put(TabelaVacina.DATA_VACINACAO, data_vacinacao)
+            put(TabelaVacina.CAMPO_ID_PACIENTE, idPaciente)
+            put(TabelaVacina.CAMPO_ID_FABRICANTE, idFabricante)
+        }
+
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Vacina {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colNum_Lote = cursor.getColumnIndex(TabelaVacina.NUM_LOTE)
+            val colData = cursor.getColumnIndex(TabelaVacina.DATA_VACINACAO)
+            val colIdPaciente = cursor.getColumnIndex(TabelaVacina.CAMPO_ID_PACIENTE)
+            val colIdFabricante = cursor.getColumnIndex(TabelaVacina.CAMPO_ID_FABRICANTE)
+
+            val id = cursor.getLong(colId)
+            val num_lote = cursor.getString(colNum_Lote)
+            val data_vacinacao = cursor.getLong(colData)
+            val idPaciente = cursor.getLong(colIdPaciente)
+            val idFabricante = cursor.getLong(colIdFabricante)
+
+
+            return Vacina(id, num_lote, data_vacinacao, idPaciente, idFabricante)
+        }
+    }
 }

bfcaaac0b741ce08ccd7b29652e2c8b2d4cdf8f9
Leonel Rita <leonelrita@hotmail.com>
2021-06-02 14:33:51 +0100

    Adicionado apagar a base de dados aos testes

--- app/src/androidTest/java/com/example/controlovacinas/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/controlovacinas/ExampleInstrumentedTest.kt
@@ -7,6 +7,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,6 +17,12 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class TestesBaseDados {
     private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
+    private fun getBdControloVacinasOpenHelper() = BdControloVacinasOpenHelper(getAppContext())
+
+    @Before
+    fun apagaBaseDados(){
+        getAppContext().deleteDatabase(BdControloVacinasOpenHelper.NOME_BASE_DADOS)
+    }
 
     @Test
     fun consegueAbrirBaseDados() {
@@ -24,4 +31,5 @@ class TestesBaseDados {
         assert(db.isOpen)
         db.close()
     }
+
 }

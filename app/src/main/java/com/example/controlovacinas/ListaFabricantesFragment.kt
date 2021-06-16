d9f128bf2412dada96697cc5e025cf0ad46f3cdf
Leonel Rita <leonelrita@hotmail.com>
2021-06-16 19:01:56 +0100

    Criado adapter de fabricantes para comunicar com o RecyclerView

--- app/src/main/java/com/example/controlovacinas/ListaFabricantesFragment.kt
+++ app/src/main/java/com/example/controlovacinas/ListaFabricantesFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.recyclerview.widget.RecyclerView
 import com.example.controlovacinas.databinding.FragmentListaFabricantesBinding
 
 
@@ -31,8 +32,9 @@ class ListaFabricantesFragment : Fragment(), LoaderManager.LoaderCallbacks<Curso
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        LoaderManager.getInstance(this)
-            .initLoader(ID_LOADER_MANAGER_FABRICANTES, null, this)
+        val recyclerViewFabricante = view.findViewById<RecyclerView>(R.id.recyclerViewFabricantes)
+
+        LoaderManager.getInstance(this).initLoader(ID_LOADER_MANAGER_FABRICANTES, null, this)
 
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }

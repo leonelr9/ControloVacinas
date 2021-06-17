906deae54bb4825e28ebdf3bcb188d33094d672e
Leonel Rita <leonelrita@hotmail.com>
2021-06-17 17:27:11 +0100

    Configurar o recyclerView e passar os dados da vacina para o AdapterVacinas

--- app/src/main/java/com/example/controlovacinas/AdapterVacinas.kt
+++ app/src/main/java/com/example/controlovacinas/AdapterVacinas.kt
@@ -1,10 +1,11 @@
 package com.example.controlovacinas
 
+import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterVacinas : RecyclerView.Adapter<AdapterVacinas.ViewHolderVacina>() {
+class AdapterVacinas(var cursor: Cursor? = null) : RecyclerView.Adapter<AdapterVacinas.ViewHolderVacina>() {
     class ViewHolderVacina(itemView: View) : RecyclerView.ViewHolder(itemView){
 
     }

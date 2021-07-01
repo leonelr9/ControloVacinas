cca0d02277c82db8e782317d61d24b29388a79c7
Leonel Rita <leonelrita@hotmail.com>
2021-07-01 14:31:11 +0100

    Ir buscar o campo Numero do lote das vacinas juntamente com os Efeitos secundarios

--- app/src/main/java/com/example/controlovacinas/TabelaEfeitosSecundarios.kt
+++ app/src/main/java/com/example/controlovacinas/TabelaEfeitosSecundarios.kt
@@ -45,7 +45,45 @@ class TabelaEfeitosSecundarios(db: SQLiteDatabase) {
         having: String?,
         orderBy: String?
     ): Cursor? {
-        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+        val ultimaColuna = columns.size - 1
+
+        var posColNumLote = -1 // -1 indica que a coluna não foi pedida
+        for (i in 0..ultimaColuna) {
+            if (columns[i] == CAMPO_EXTERNO_NUM_LOTE) {
+                posColNumLote = i
+                break
+            }
+        }
+
+        if (posColNumLote == -1) {
+            return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+        }
+
+        var colunas = ""
+        for (i in 0..ultimaColuna) {
+            if (i > 0) colunas += ","
+
+            colunas += if (i == posColNumLote) {
+                "${TabelaVacina.NOME_TABELA}.${TabelaVacina.NUM_LOTE} AS $CAMPO_EXTERNO_NUM_LOTE"
+            } else {
+                "${NOME_TABELA}.${columns[i]}"
+            }
+        }
+
+        val tabelas = "$NOME_TABELA INNER JOIN ${TabelaVacina.NOME_TABELA} ON ${TabelaVacina.NOME_TABELA}.${BaseColumns._ID}=$CAMPO_ID_VACINA"
+
+        var sql = "SELECT $colunas FROM $tabelas"
+
+        if (selection != null) sql += " WHERE $selection"
+
+        if (groupBy != null) {
+            sql += " GROUP BY $groupBy"
+            if (having != null) " HAVING $having"
+        }
+
+        if (orderBy != null) sql += " ORDER BY $orderBy"
+
+        return db.rawQuery(sql, selectionArgs)
     }
 
     companion object{
@@ -59,7 +97,8 @@ class TabelaEfeitosSecundarios(db: SQLiteDatabase) {
         const val DOR_BRACO = "dor_no_braco"
         const val OUTRO = "outro"
         const val CAMPO_ID_VACINA = "id_vacina"
+        const val CAMPO_EXTERNO_NUM_LOTE = "num_lote"
 
-        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, FEBRE, FADIGA, DOR_CABECA, DORES_MOSCULARES, CALAFRIOS, DIARREIA, DOR_BRACO, OUTRO, CAMPO_ID_VACINA)
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, FEBRE, FADIGA, DOR_CABECA, DORES_MOSCULARES, CALAFRIOS, DIARREIA, DOR_BRACO, OUTRO, CAMPO_ID_VACINA, CAMPO_EXTERNO_NUM_LOTE)
     }
 }

5665cf3b42b7843ac5b22e024465c2a0b6f8915b
Leonel Rita <leonelrita@hotmail.com>
2021-07-05 19:05:59 +0100

    Criado interface para editar o paciente Adicionado inputType nos editText do novo paciente

--- app/src/main/java/com/example/controlovacinas/EditaPacienteFragment.kt
+++ app/src/main/java/com/example/controlovacinas/EditaPacienteFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [edita_Paciente_Fragment.newInstance] factory method to
+ * Use the [EditaPacienteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class edita_Paciente_Fragment : Fragment() {
+class EditaPacienteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class edita_Paciente_Fragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            edita_Paciente_Fragment().apply {
+            EditaPacienteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)

2174bbd59698b80bb7c96296ed1616952d83fd5b
Leonel Rita <leonelrita@hotmail.com>
2021-07-05 15:12:15 +0100

    Criado interface para inserir novos pacientes

---
+++ app/src/main/java/com/example/controlovacinas/NovoPacienteFragment.kt
@@ -0,0 +1,27 @@
+package com.example.controlovacinas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NovoPacienteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NovoPacienteFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_novo_paciente, container, false)
+    }
+    
+}

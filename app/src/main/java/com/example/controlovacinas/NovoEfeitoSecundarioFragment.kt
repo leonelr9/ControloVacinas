6763821fdeec12702f3230420e87e38250d1edab
Leonel Rita <leonelrita@hotmail.com>
2021-07-07 09:56:25 +0100

    Criadas variáveis para as views contidas ne NovoEfeitoSecundarioFragment Ajuste no layout do novo efeito secundario

--- app/src/main/java/com/example/controlovacinas/NovoEfeitoSecundarioFragment.kt
+++ app/src/main/java/com/example/controlovacinas/NovoEfeitoSecundarioFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 import com.example.controlovacinas.databinding.FragmentNovaVacinaBinding
 import com.example.controlovacinas.databinding.FragmentNovoEfeitoSecundarioBinding
@@ -19,6 +22,17 @@ class NovoEfeitoSecundarioFragment : Fragment() {
 
     private var _binding: FragmentNovoEfeitoSecundarioBinding? = null
 
+    private lateinit var spinnerVacina: Spinner
+    private lateinit var checkBoxFebre: CheckBox
+    private lateinit var checkBoxFadiga: CheckBox
+    private lateinit var checkBoxDorCabeca: CheckBox
+    private lateinit var checkBoxDoresMoscular: CheckBox
+    private lateinit var checkBoxCalafrios: CheckBox
+    private lateinit var checkBoxDiarreia: CheckBox
+    private lateinit var checkBoxDorBraco: CheckBox
+    private lateinit var editTextOutros: EditText
+
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,6 +44,20 @@ class NovoEfeitoSecundarioFragment : Fragment() {
 
         _binding = FragmentNovoEfeitoSecundarioBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        spinnerVacina = view.findViewById(R.id.spinnerVacina)
+        checkBoxFebre = view.findViewById(R.id.checkBoxFebre)
+        checkBoxFadiga = view.findViewById(R.id.checkBoxFadiga)
+        checkBoxDorCabeca = view.findViewById(R.id.checkBoxDorCabeca)
+        checkBoxDoresMoscular = view.findViewById(R.id.checkBoxDoresMosculares)
+        checkBoxCalafrios = view.findViewById(R.id.checkBoxCalafrios)
+        checkBoxDiarreia = view.findViewById(R.id.checkBoxDiarreia)
+        checkBoxDorBraco = view.findViewById(R.id.checkBoxDorBraco)
+        editTextOutros = view.findViewById(R.id.editTextOutros)
     }
 
     override fun onDestroyView() {
